bb2a37d19bd3cac9496d4cdbb4a3ede626309268
alexHooper <alexmohack@gmail.com>
2018-08-13 20:01:08 +0530

    add url for home page

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import contact_view
+from .views import (
+	contact_view,
+	index,
+)
 
 urlpatterns = [
 	path('contact/', contact_view, name='contact-form'),
+	path('', index, name='home-page'),
 ]
