6a89190de80bb7debef56c549cecb64884d85e3d
alexHooper <alexmohack@gmail.com>
2018-08-13 23:48:06 +0530

    add method for logging when form is submitted

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,5 @@
-"use strict";
-
-formData = {
-	'contact_name': $("input[name=name]").val(),
-	'contact_email': $("input[name=email]").val(),
-	'contact_message': $("input[name=message]").val()
-}
-
-$("#send").onclick = send_contact_form();
-
-function send_contact_form(data) {
-	$("#status").html("Sending...");
-
-	$.ajax({
-		url: {% url 'contact-form' %},
-		type: "POST",
-		data: formData,
-		success: function(data, textStatus, jqXHR) {
-			$("#status").text(data.valid_data),
-			if (data.valid_data) {
-				$('#contact-form').closest('form').find("input[type=text], textarea").val("");
-			}
-			$("#status").html(data.valid_data);
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			$("#status").text(jqXHR);
-		}
-	})
-}
+$("#contact-form").on("submit", function(event) {
+	event.preventDefault();
+	console.log("Form Submitted");
+	send_contact_form();
+})
