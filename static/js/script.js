4891f82eea8c678abd06766f56eb0ea2e323f269
alexHooper <alexmohack@gmail.com>
2018-08-14 00:09:28 +0530

    create function for ajax request

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,12 @@
+// form submission
 $("#contact-form").on("submit", function(event) {
 	event.preventDefault();
 	console.log("Form Submitted");
 	send_contact_form();
-})
+});
+
+// AJAX for contact submission
+function send_contact_form() {
+	console.log("Contact form is working!");
+	console.log($("#id_name").val());
+}
