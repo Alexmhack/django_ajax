e46c9fecb73e976956f063d824e0cfcbf92bbf7e
alexHooper <alexmohack@gmail.com>
2018-08-13 14:57:15 +0530

    add code for our contact page view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.http import JsonResponse
 
-# Create your views here.
+from .forms import ContactForm
+from .models import UserContactInfo
+
+data = {
+	'valid_data': 'Thanks for contacting with us!',
+	'invalid_name': 'Please enter a valid name',
+	'invalid_email': 'Please enter a valid email',
+	'invalid_message': 'Please enter a valid message',
+}
+
+def contact_view(request):
+	contact_form = ContactForm
+	if request.method == "POST":
+		form = contact_form(data=request.POST)
+		if form.is_valid():
+			contact_name = request.POST.get("contact_name", "")
+			contact_email = request.POST.get("contact_email", "")
+			contact_message = request.POST.get("contact_message", "")
+
+			template = get_template("contact_email.txt")
+			context = {
+				'contact_name': contact_name,
+				'contact_email': contact_email,
+				'contact_message': contact_message
+			}
+			content = template.render(context)
+			email = EmailMessage(
+				"New Contact Form Submission",
+				content,
+				"Django AJAX WebApp",
+				['primaryeaddress@gmail.com'],
+			)
+			email.send()
+			data['contact_name'] = contact_name
+			UserContactInfo.objects.create(
+				name=contact_name,
+				email=contact_email,
+				message=contact_message
+			)
+			return JsonResponse(data)
+		else:
+			return JsonResposne(data['invalid_email'])
+	return render(request, "contact_form.html")
