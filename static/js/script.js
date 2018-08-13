837e644f4b66df1481f55dbad27955b0d54c0c32
alexHooper <alexmohack@gmail.com>
2018-08-13 20:58:05 +0530

    create function for sending ajax request with our form details

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+formData = {
+	'contact_name': $("input[name=name]").val(),
+	'contact_email': $("input[name=email]").val(),
+	'contact_message': $("input[name=message]").val()
+}
+
+function send_contact_form(data) {
+	$("#status").html("Sending...");
+
+	$.ajax({
+		url: {% url 'contact-form' %},
+		type: "POST",
+		data: formData,
+		success: function(data, textStatus, jqXHR) {
+			$("#status").text(data.valid_data),
+			if (data.valid_data) {
+				$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			$("#status").text(jqXHR);
+		}
+	})
+}
