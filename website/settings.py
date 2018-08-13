9e3e1d557fefe4dabdd031832babbd0de728a9a5
alexHooper <alexmohack@gmail.com>
2018-08-13 13:57:19 +0530

    fetch secret key from env.json file

--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@=astm3lsi36_1gp-z1(!sjl=8j5k!l16v_%cfjp2ld6janas6'
+with open("env.json") as f:
+    data = json.load(f)
+
+SECRET_KEY = data['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
