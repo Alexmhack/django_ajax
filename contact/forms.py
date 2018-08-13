a43a7c18228a73e974693d7acfaa7ae417799c06
alexHooper <alexmohack@gmail.com>
2018-08-13 20:13:11 +0530

    minor changes

--- contact/forms.py
+++ contact/forms.py
@@ -3,4 +3,8 @@ from django import forms
 class ContactForm(forms.Form):
 	name = forms.CharField(max_length=30, required=True, label="Your name: ")
 	email = forms.EmailField(required=True, label="Your email: ")
-	message = forms.CharField(widget=forms.Textarea, required=True, label="Your message: ")
+	message = forms.CharField(
+		widget=forms.Textarea,
+		required=True,
+		label="Your message: ",
+	)
