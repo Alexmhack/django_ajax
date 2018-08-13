8d548d9fe29c8b2e9e75e9f5f5a12e87922d620d
alexHooper <alexmohack@gmail.com>
2018-08-14 00:24:09 +0530

    add logic for ajax request

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,35 @@ $("#contact-form").on("submit", function(event) {
 	send_contact_form();
 });
 
+formData = {
+	'contact_name': $("input[name=name]").val(),
+	'contact_email': $("input[name=email]").val(),
+	'contact_message': $("input[name=message]").val()
+}
+
 // AJAX for contact submission
 function send_contact_form() {
 	console.log("Contact form is working!");
 	console.log($("#id_name").val());
+	console.log($("#id_email").val());
+	console.log($("#contact_message").val());
+
+	$("#status").html("Sending...")
+
+	$.ajax({
+		url: {% contact-form %},
+		type: "POST",
+		data: formData
+		success: function(data) {
+			$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+			console.log(data);
+			console.log("success");
+			$("#status").html(data.valid_data);
+		},
+		error : function(xhr,errmsg,err) {
+            $('#status').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+	})
 }
