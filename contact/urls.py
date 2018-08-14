54ae6033ed0990b8414dcb55414d59290a099bf9
alexHooper <alexmohack@gmail.com>
2018-08-14 23:31:26 +0530

    add url for sending email view

--- contact/urls.py
+++ contact/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
 	contact_view,
+	send_email,
 	index,
 )
 
 urlpatterns = [
 	path('contact/', contact_view, name='contact-form'),
+	path('send-email/', send_email, name='send-email'),
 	path('', index, name='home-page'),
 ]
