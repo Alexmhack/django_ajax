18d22720a596c5589bbe5e46b095f63723b2b47f
alexHooper <alexmohack@gmail.com>
2018-08-14 23:31:53 +0530

    add custom view for sending email

--- contact/views.py
+++ contact/views.py
@@ -15,37 +15,35 @@ data = {
 
 def contact_view(request):
 	contact_form = ContactForm
-	if request.method == "POST":
-		form = contact_form(data=request.POST)
-		if form.is_valid():
-			contact_name = request.POST.get("name", "")
-			contact_email = request.POST.get("email", "")
-			contact_message = request.POST.get("message", "")
-
-			template = get_template("contact_email.txt")
-			context = {
-				'contact_name': contact_name,
-				'contact_email': contact_email,
-				'contact_message': contact_message
-			}
-			content = template.render(context)
-			email = EmailMessage(
-				"New Contact Form Submission",
-				content,
-				"Django AJAX WebApp",
-				['primaryeaddress@gmail.com'],
-			)
-			email.send()
-			data['contact_name'] = contact_name
-			UserContactInfo.objects.create(
-				name=contact_name,
-				email=contact_email,
-				message=contact_message
-			)
-			return JsonResponse(data)
-		else:
-			return JsonResposne(data['invalid_email'])
 	return render(request, "contact/contact_form.html", {'form': contact_form})
+
+
+def send_email(request):
+	contact_name = request.GET.get("contact_name", "")
+	contact_email = request.GET.get("contact_email", "")
+	contact_message = request.GET.get("contact_message", "")
+
+	template = get_template("contact_email.txt")
+	context = {
+		'contact_name': contact_name,
+		'contact_email': contact_email,
+		'contact_message': contact_message
+	}
+	content = template.render(context)
+	email = EmailMessage(
+		"New Contact Form Submission",
+		content,
+		"Django AJAX WebApp",
+		['primaryeaddress@gmail.com'],
+	)
+	email.send()
+	data['contact_name'] = contact_name
+	UserContactInfo.objects.create(
+		name=contact_name,
+		email=contact_email,
+		message=contact_message
+	)
+	return JsonResponse(data)
 
 
 def index(request):
